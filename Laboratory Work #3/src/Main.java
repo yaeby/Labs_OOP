00b0edd2ff48634828920589dd3d5b875a05e582
yaeby <adrian.copta@isa.utm.md>
2023-11-13 23:38:02 +0200

    Added queue and implementations

--- Laboratory Work #3/src/Main.java
+++ Laboratory Work #3/src/Main.java
@@ -1,4 +1,16 @@
+import Queue.ListQueue;
+import Queue.Queue;
+
 public class Main {
     public static void main(String[] args) {
+        Queue<Integer> stack = new ListQueue<Integer>();
+
+        stack.enqueue(10);
+        stack.enqueue(20);
+        stack.enqueue(30);
+
+        System.out.println(stack.dequeue());
+        System.out.println(stack.peek());
+        System.out.println(stack.size());
     }
 }
---
+++ Laboratory Work #3/src/Queue/ListQueue.java
@@ -0,0 +1,45 @@
+package Queue;
+
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+
+public class ListQueue<T> implements Queue<T> {
+    private final ArrayList<T> items;
+    private int front;
+
+    public ListQueue() {
+        items = new ArrayList<>();
+        front = 0;
+    }
+
+    @Override
+    public void enqueue(T item) {
+        items.add(item);
+    }
+
+    @Override
+    public T dequeue() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("Queue is empty");
+        }
+        return items.remove(front);
+    }
+
+    @Override
+    public T peek() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("Queue is empty");
+        }
+        return items.get(front);
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return front == items.size();
+    }
+
+    @Override
+    public int size() {
+        return items.size() - front;
+    }
+}
