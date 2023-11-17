15a2ef25215cf1c3b48922e517acfd5638568885
yaeby <adrian.copta@isa.utm.md>
2023-11-17 11:16:54 +0200

    Created Stack class and its implementations

---
+++ Laboratory Work #3/src/Stack/LinkedListStack.java
@@ -0,0 +1,52 @@
+package Stack;
+
+import Node.Node;
+
+import java.util.NoSuchElementException;
+
+public class LinkedListStack<T> implements Stack<T> {
+    private Node<T> top;
+    private int size;
+
+    public LinkedListStack() {
+        top = null;
+        size = 0;
+    }
+
+    @Override
+    public void push(T item) {
+        Node<T> newNode = new Node<>(item);
+        newNode.next = top;
+        top = newNode;
+        size++;
+    }
+
+    @Override
+    public T pop() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("Stack is empty");
+        }
+        T value = top.data;
+        top = top.next;
+        size--;
+        return value;
+    }
+
+    @Override
+    public T peek() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("Stack is empty");
+        }
+        return top.data;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return top == null;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+}
---
+++ Laboratory Work #3/src/Stack/Stack.java
@@ -0,0 +1,9 @@
+package Stack;
+
+public interface Stack<T> {
+    void push(T element);
+    T pop();
+    T peek();
+    boolean isEmpty();
+    int size();
+}
