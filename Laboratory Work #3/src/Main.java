47a8954c9577d88102d3cf3afd426db228bca461
yaeby <adrian.copta@isa.utm.md>
2023-12-05 14:29:08 +0200

    Made some testes in main to test the queue and stack implementations

--- Laboratory Work #3/src/Main.java
+++ Laboratory Work #3/src/Main.java
@@ -1,16 +1,50 @@
 import Queue.ListQueue;
 import Queue.Queue;
+import Queue.ArrayQueue;
+import Queue.Queue;
+import Stack.LinkedListStack;
+import Stack.ListStack;
+import Stack.LinkedListStack;
+import Stack.ArrayStack;
+import Stack.Stack;
+import Stack.ListStack;
 
 public class Main {
     public static void main(String[] args) {
-        Queue<Integer> stack = new ListQueue<Integer>();
+        //Testes
+        Queue<Integer> queue = new ListQueue<Integer>();
+        queue.enqueue(10);
+        queue.enqueue(20);
+        queue.enqueue(30);
+        System.out.println(queue.dequeue());
+        System.out.println(queue.peek());
+        System.out.println(queue.size() + "\n");
 
-        stack.enqueue(10);
-        stack.enqueue(20);
-        stack.enqueue(30);
+        Queue<String> queue2 = new ArrayQueue<String>(3);
+        queue2.enqueue("String one");
+        queue2.enqueue("String two");
+        queue2.enqueue("String tree");
+        System.out.println(queue2.dequeue());
+        System.out.println(queue2.peek());
+        System.out.println(queue2.dequeue());
+        System.out.println(queue2.dequeue() + "\n");
+        //System.out.println(queue2.dequeue());
 
-        System.out.println(stack.dequeue());
+        Stack<Integer> stack = new ListStack<Integer>();
+        stack.push(777);
+        stack.push(888);
         System.out.println(stack.peek());
+        System.out.println(stack.isEmpty());
         System.out.println(stack.size());
+        System.out.println(stack.pop());
+        System.out.println(stack.size());
+        System.out.println(stack.pop()+"\n");
+
+        Stack<String> stack2 = new LinkedListStack<String>();
+        stack2.push("El 1");
+        stack2.push("EL 2");
+        System.out.println(stack2.pop());
+        System.out.println(stack2.peek());
+        System.out.println(stack2.pop());
     }
 }
